32ce782d89da4430a4017c850b6e2f9a39aee8cf
jansenzhao <zhaoyansheng163@163.com>
2025-08-29 16:13:15 +0800

    1

--- fox-mock-agent/src/main/java/com/cxytiandi/foxmock/agent/other/AgentSocketServer.java
+++ fox-mock-agent/src/main/java/com/cxytiandi/foxmock/agent/other/AgentSocketServer.java
@@ -1,9 +1,14 @@
 package com.cxytiandi.foxmock.agent.other;
 
+import org.example.utils.SpringUtils;
+import org.springframework.beans.factory.support.DefaultListableBeanFactory;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 import java.net.ServerSocket;
 import java.net.Socket;
 
@@ -20,13 +25,21 @@ public class AgentSocketServer {
                         // 读取客户端发送的指令
                         String command = in.readLine();
                         System.out.println("[Agent] Received command: " + command);
+                        DefaultListableBeanFactory beanFactory = SpringUtils.getBeanFactory();
+                        //  invoke:com.example.TargetClass:targetMethod
+                        String[] parts = command.split(":");
+                        String className = parts[1];
+                        String methodName = parts[2];
+                        Object target = beanFactory.getBean(className);
+                        Method method = target.getClass().getMethod(methodName);
+                        method.invoke(target);
 
                         // 解析和执行指令
                         String response = executeCommand(command);
 
                         // 将执行结果返回给客户端
                         out.println(response);
-                    } catch (IOException e) {
+                    } catch (IOException | NoSuchMethodException | IllegalAccessException | InvocationTargetException e) {
                         e.printStackTrace();
                     }
                 }
